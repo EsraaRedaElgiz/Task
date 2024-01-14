a75da4da4df272ddc676dd6d03eab9c8d57b683f
EsraaRedaElgiz <esraaelgiz@gmail.com>
2024-01-14 11:38:30 +0200

    first

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Task from './src/screens/task';
+import { StatusBar } from 'react-native';
+import { COLORS } from './src/constants/Constants';
+const App = () => {
+
+
+  return (
+    <>
+      <StatusBar backgroundColor={COLORS.white} />
+      <Task />
+    </>
+  );
+}
+
+export default App;
